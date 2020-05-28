bd2c2c8452b8e2fe77ecb148e5779add86c61178
chenzq <aqiuiscoming@163.com>
2020-05-28 09:50:45 +0800

    加新修改时间

--- src/cn/tldeu/lx/HW.java
+++ src/cn/tldeu/lx/HW.java
@@ -5,7 +5,7 @@ public class HW {
 	public static void main(String[] args) {
 		System.out.println("The world is beautiful!");
 		System.out.println("Hello World!");
-
+		System.out.println("2020-05-28");
 	}
 
 }
